23b52b66a1757d36c9e630e7a2b14f0fb40cae0a
Natcancein <nataliacancino@MacBook-Pro-de-Natalia.local>
2017-12-14 09:31:25 -0300

    actualización reto lyft app

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,27 @@
 $(document).ready(function(){
 
-// inicio desapareción logo
+//ANIMACION PRINCIPIO
+
+$('#section1').hide(3000);
+ //inicio desapareción logo
 	 setTimeout(function() {
-        $('.logo').fadeOut(2500);
-    },2000);
-
-
- 
- $('.section1').hide(3000);
+        $('#logo').fadeOut(2500);
+    },3000);
 
   setTimeout(function() {
-        $('.section1').fadeIn(2500);
+        $('#section1').fadeIn(2500);
     },2000);
 
  //comprobar codigo numero de teléfono
-$("#enviar").submit(function () {  
-    if($("#telefono").val().length < 1) {  
+$("#enviar").click(function () {  
+    if($("#telefono").val().length == " ") {  
         alert("El teléfono es obligatorio");  
         return false;  
     }  
     return false;  
 }); 
 
-$("#enviar").submit(function () {  
+$("#enviar").click(function () {  
     if(isNaN($("#telefono").val())) {  
         alert("El teléfono solo debe contener números");  
         return false;  
@@ -30,21 +29,18 @@ $("#enviar").submit(function () {
     return false;  
 });  
 
-$("#enviar").submit(function () {  
-    if($("#telefono").val().length < 9) {  
-        alert("El teléfono debe tener 10 caracteres. Ej. 6661122339");  
-        return false;  
-    }  
-    return false;  
-}); 
+// BOTON NEXT
 
-var value = $('#telefono').val();
-   if(value.length() != 10 || value.length() = ''){
-     $('#enviar').addClass('disabled'); //desabilitar boton
-   }
-   if(value.size() == 10){
-     $('#enviar').removeClass('disabled'); // remover
-   };
+$("#telefono").keyup(function(){
+	if ($(this).val().length<10){
+		$("#enviar").attr("class","disabled");
+	}else {
+		$("#enviar").removeAttr("class", "disabled");
+		$("#enviar").attr("id", "btn-next");
+	} 
+
+})
+
 
 });
 
