06f063ffacc8ab0977002c1c4b300877fe15a312
Natcancein <nataliacancino@MacBook-Pro-de-Natalia.local>
2017-12-13 21:33:41 -0300

    actualizacion jquery reto

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,8 +12,39 @@ $(document).ready(function(){
   setTimeout(function() {
         $('.section1').fadeIn(2500);
     },2000);
- 
-if $("#formulario") ===
+
+ //comprobar codigo numero de teléfono
+$("#enviar").submit(function () {  
+    if($("#telefono").val().length < 1) {  
+        alert("El teléfono es obligatorio");  
+        return false;  
+    }  
+    return false;  
+}); 
+
+$("#enviar").submit(function () {  
+    if(isNaN($("#telefono").val())) {  
+        alert("El teléfono solo debe contener números");  
+        return false;  
+    }  
+    return false;  
+});  
+
+$("#enviar").submit(function () {  
+    if($("#telefono").val().length < 9) {  
+        alert("El teléfono debe tener 10 caracteres. Ej. 6661122339");  
+        return false;  
+    }  
+    return false;  
+}); 
+
+var value = $('#telefono').val();
+   if(value.length() != 10 || value.length() = ''){
+     $('#enviar').addClass('disabled'); //desabilitar boton
+   }
+   if(value.size() == 10){
+     $('#enviar').removeClass('disabled'); // remover
+   };
 
 });
 
